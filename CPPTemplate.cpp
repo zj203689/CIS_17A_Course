d577e6277f896372ef602abdbd5686d9ebcbbde8
Jimmy Jiang <jj2036896647@berkeley.edu>
2026-02-23 14:07:01 -0800

    Add files via upload

---
+++ CPPTemplate.cpp
@@ -0,0 +1,34 @@
+/*
+Author:  Zhongjie Jiang
+Date:	 February , 2026
+Purpose: 
+*/
+
+//System Libraries
+#include <iostream>//input/output library
+#include <cstdlib> //random number library
+#include <ctime>   //time library
+using namespace std;
+
+//User Libraries
+
+//Global Constants - Physics/Chemistry/Conversions/Array Diamensions
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argv,char **argc) {
+	//set the random number seed 
+	srand(static_cast<unsigned int>(time(0)));
+	//Declear Variables
+	
+	//Initialize Variables
+	
+	//Mapping process -> Input to Outputs
+	
+	//display input/outputs
+	
+	//exit the program
+	return 0;
+	
+};
